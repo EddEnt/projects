10158c18a72f93f5c040a1c32ecff5135e79f1a7
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-11-13 16:07:20 -0500

    Capture user entered numbers and initial console UI for main menu.

--- CalculatorApp/Calculator/Program.cs
+++ CalculatorApp/Calculator/Program.cs
@@ -1,4 +1,23 @@
 ﻿
 
+int num1 = 0;
+int num2 = 0;
+
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+Console.WriteLine("Type in a number, and then press Enter...");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in another number, and then press Enter...");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("What operation would you like to perform?:  ");
+
 
 
