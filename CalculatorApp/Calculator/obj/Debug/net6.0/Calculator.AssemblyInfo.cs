fd3c25d297be65ce16561b6910032928b87c94e6
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-11-15 11:18:32 -0500

    Divide by Zero error Handling

--- CalculatorApp/Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
+++ CalculatorApp/Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb1f2a8636ad0d6f4b790a1c953ff9ca3871080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3da8ef05f2c08398f35e33103d874e9bf1eefa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
