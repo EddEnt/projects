bfd47dde7badf76dda7f197fd78046b766d26861
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-11-15 10:56:41 -0500

    Int to Double Change
    Refactored Calculator to use Double instead of Int

--- CalculatorApp/Calculator/Program.cs
+++ CalculatorApp/Calculator/Program.cs
@@ -2,17 +2,17 @@
 
 using System.Threading.Channels;
 
-float num1 = 0;
-float num2 = 0;
+double num1 = 0;
+double num2 = 0;
 
 Console.WriteLine("Console Calculator in C#\r");
 Console.WriteLine("------------------------\n");
 
 Console.WriteLine("Type in a number, and then press Enter...");
-num1 = Convert.ToInt32(Console.ReadLine());
+num1 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Type in another number, and then press Enter...");
-num2 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Choose an option from the following list:");
 Console.WriteLine("\ta - Add");
--- CalculatorApp/Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
+++ CalculatorApp/Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06e44d29497c052b7bfd51d2c9ff2395d7dc449")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb1f2a8636ad0d6f4b790a1c953ff9ca3871080")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
