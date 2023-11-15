3dda245dc79348a90c93352be5e791788bf6c969
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-11-15 11:46:58 -0500

    Seperated Calculator functions into own class

--- CalculatorApp/Calculator/Program.cs
+++ CalculatorApp/Calculator/Program.cs
@@ -1,48 +1,69 @@
 ﻿
 
-using System.Threading.Channels;
-
-double num1 = 0;
-double num2 = 0;
-
+bool endApp = false;
+// Display title as the C# console calculator app.
 Console.WriteLine("Console Calculator in C#\r");
 Console.WriteLine("------------------------\n");
 
-Console.WriteLine("Type in a number, and then press Enter...");
-num1 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Type in another number, and then press Enter...");
-num2 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Choose an option from the following list:");
-Console.WriteLine("\ta - Add");
-Console.WriteLine("\ts - Subtract");
-Console.WriteLine("\tm - Multiply");
-Console.WriteLine("\td - Divide");
-Console.WriteLine("\tq - Quit");
-Console.Write("What operation would you like to perform?:  ");
-
-switch (Console.ReadLine())
+while (!endApp)
 {
-    case "a":
-        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2) );
-        break;
-    case "s":
-        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-        break;
-    case "m":
-        Console.WriteLine($"Your result: {num1} x {num2} = " + (num1 * num2));
-        break;
-    case "d":
-        while (num2 == 0)
-        {
-            Console.WriteLine("Enter a non-zero divisor: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-        }
-        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-        break;
-    case "q":
-        Environment.ExitCode = 0;
-        break;
-}
+    // Declare variables and set to empty.
+    string numInput1 = "";
+    string numInput2 = "";
+    double result = 0;
 
+    // Ask the user to type the first number.
+    Console.Write("Type a number, and then press Enter: ");
+    numInput1 = Console.ReadLine();
+
+    double cleanNum1 = 0;
+    while (!double.TryParse(numInput1, out cleanNum1))
+    {
+        Console.Write("This is not valid input. Please enter an integer value: ");
+        numInput1 = Console.ReadLine();
+    }
+
+    // Ask the user to type the second number.
+    Console.Write("Type another number, and then press Enter: ");
+    numInput2 = Console.ReadLine();
+
+    double cleanNum2 = 0;
+    while (!double.TryParse(numInput2, out cleanNum2))
+    {
+        Console.Write("This is not valid input. Please enter an integer value: ");
+        numInput2 = Console.ReadLine();
+    }
+
+    // Ask the user to choose an operator.
+    Console.WriteLine("Choose an operator from the following list:");
+    Console.WriteLine("\ta - Add");
+    Console.WriteLine("\ts - Subtract");
+    Console.WriteLine("\tm - Multiply");
+    Console.WriteLine("\td - Divide");
+    Console.Write("Your option? ");
+
+    string op = Console.ReadLine();
+
+    try
+    {
+        result = Calculator.Calculator.DoOperation(cleanNum1, cleanNum2, op);
+        if (double.IsNaN(result))
+        {
+            Console.WriteLine("This operation will result in a mathematical error.\n");
+        }
+        else Console.WriteLine("Your result: {0:0.##}\n", result);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+    }
+
+    Console.WriteLine("------------------------\n");
+
+    // Wait for the user to respond before closing.
+    Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+    if (Console.ReadLine() == "n") endApp = true;
+
+    Console.WriteLine("\n"); // Friendly linespacing.
+}
+return;
