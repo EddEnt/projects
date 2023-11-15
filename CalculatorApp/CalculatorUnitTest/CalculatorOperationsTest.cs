37ab55e3b28c505699858b1674f77b8b0182b054
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-11-15 14:42:21 -0500

    Fixed mistype of extra closing bracket

--- CalculatorApp/CalculatorUnitTest/CalculatorOperationsTest.cs
+++ CalculatorApp/CalculatorUnitTest/CalculatorOperationsTest.cs
@@ -8,7 +8,17 @@ namespace CalculatorUnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+            Calculator calculator = new Calculator();
+            double num1 = 5;
+            double num2 = 7;
+            string op = "a";
 
+            // Act
+            double result = calculator.DoOperation(num1, num2, op);
+
+            // Assert
+            Assert.AreEqual(12, result);
         }
     }
 }
