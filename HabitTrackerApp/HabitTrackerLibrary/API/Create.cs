ef135e140141ff06e12004d3342913bbe6551381
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-11-28 09:08:55 -0500

    HabitTrackerLibrary created. Create.cs record method in progress.

---
+++ HabitTrackerApp/HabitTrackerLibrary/API/Create.cs
@@ -0,0 +1,43 @@
+﻿namespace HabitTrackerLibrary.api
+{
+    public class Create
+    {
+
+        public static void InsertRecord()
+        {
+            string date = GetDateInput();
+        }
+
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("Please enter the current date: (dd-mm-yy). Enter '0' to return to the Main Menu... ");
+            string dateInput = Console.ReadLine();
+
+            if (dateInput == "0")
+            {
+                //GetUserInput();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again...");
+            }
+
+            return dateInput;
+            
+        }
+
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+            string numberInput = Console.ReadLine();
+
+            if (numberInput == "0")
+            {
+
+            }
+        }
+
+    }
+
+
+}
