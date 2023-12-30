07a19203620ee860ef23ecf0df05cbb9d40c383a
Edward Entecott <61289536+EddEnt@users.noreply.github.com>
2023-12-30 16:04:06 -0500

    Customer class and Constructor complete

--- AirplaneManagement/LibraryAirplaneManagement/Constructors/Customer.cs
+++ AirplaneManagement/LibraryAirplaneManagement/Constructors/Customer.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace LibraryAirplaneManagement.Constructors
 {
-    internal class Customer
+    public class Customer
     {
+        public int CustomerId {  get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public int Bookings { get; private set; }
+
+        public Customer(int customerId, string firstName, string lastName, string phoneNumber)
+        {
+            Bookings = 0;
+            CustomerId = customerId;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+
+        
+        public string toString()
+        {
+            string customerString = 
+                $"Customer: {CustomerId}\nName: {LastName}, {FirstName}\nPhone: {PhoneNumber}\nBookings: {Bookings}";
+            return customerString;
+        }
+
     }
 }
