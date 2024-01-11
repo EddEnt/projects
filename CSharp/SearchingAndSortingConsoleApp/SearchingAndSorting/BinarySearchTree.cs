ab6b537c01a1dd0e5dc8d1fe13e4bf2a9bb68e24
EddEnt <61289536+EddEnt@users.noreply.github.com>
2024-01-11 09:26:23 -0500

    Binary search tree

---
+++ CSharp/SearchingAndSortingConsoleApp/SearchingAndSorting/BinarySearchTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingAndSorting
+{
+    public class BinarySearchTree
+    {
+        //Class cotaining the node structure
+        public class Node
+        {
+            public int key;
+            public Node left, right;
+
+            public Node(int item)
+            {
+                key = item;
+                left = right = null;
+            }
+        }
+
+        Node root;
+
+        //Constructor
+        public BinarySearchTree() { root = null; }
+        public BinarySearchTree(int value) { root = new Node(value); }
+
+        //Calling our recursive function, InsertRecursive
+        public void Insert(int key)
+        {
+            root = InsertRecursive(root, key);
+        }
+
+        //Inserting a new node into the Binary Search Tree
+        Node InsertRecursive(Node root, int key)
+        {
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+            if(key < root.key)
+            {
+                root.left = InsertRecursive(root.left, key);
+            }
+            else if (key > root.key)
+            {
+                root.right = InsertRecursive(root.right, key);
+            }
+
+            return root;
+        }
+
+        //Calling our recursive function, InOrderTraversalRecursive
+        public void InOrderTraversal() 
+        { 
+            InOrderTraversalRecursive(root); 
+        }
+
+        //In-Order Traversal of the Binary Search Tree
+        public void InOrderTraversalRecursive(Node root)
+        {
+            if (root != null)
+            {
+                InOrderTraversalRecursive(root.left);
+                Console.Write($"{root.key} ");
+                InOrderTraversalRecursive(root.right);
+            }
+        }
+
+
+    }
+}
---
+++ CSharp/SearchingAndSortingConsoleApp/SearchingAndSorting/Program.cs
@@ -0,0 +1,57 @@
+﻿using SearchingAndSorting;
+
+string userInput = String.Empty;
+int userNumber = 0;
+bool userInputIsValid = false;
+List<int> numbers = new();
+numbers.Add(1);
+numbers.Add(23);
+
+Console.WriteLine("--- Searching and Sorting ---");
+/*
+Console.Write($"Current numbers in the List: ");
+for (int i = 0; i < numbers.Count; i++)
+{
+    
+    if(i != numbers.Count - 1)
+    {
+        Console.Write($"{numbers[i]}, ");
+    }
+    else
+    {
+        Console.Write($"{numbers[i]}");
+    }
+}
+Console.WriteLine();
+
+while (!userInputIsValid)
+{
+    Console.Write("Enter a number to add to our List: ");
+    userInput = Console.ReadLine();
+    userInputIsValid = int.TryParse(userInput, out userNumber);
+    if(!userInputIsValid)
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number...\n");
+    }
+    else
+    {
+        numbers.Add(userNumber);
+    }
+
+}
+*/
+
+BinarySearchTree tree = new();
+tree.Insert(50);
+tree.Insert(30);
+tree.Insert(20);
+tree.Insert(40);
+tree.Insert(70);
+tree.Insert(60);
+tree.Insert(80);
+
+Console.Write($"In-Order Traversal of the Binary Search Tree: ");
+tree.InOrderTraversal();
+
+
+
